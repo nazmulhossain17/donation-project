10304d92c2b476659d195a8a1d12b6b1620c5ebb
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-02-08 12:49:10 +0600

    navbar

---
+++ src/app/donation/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+
+const Donation = () => {
+  return (
+    <div className="mt-20">
+      <h1>Donation page</h1>
+    </div>
+  )
+}
+
+export default Donation
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { HiMenu } from "react-icons/hi";
+
+function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const [isSticky, setIsSticky] = useState<boolean>(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 0) {
+          setIsSticky(true);
+        } else {
+          setIsSticky(false);
+        }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+      
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+  
+    return (
+      <>
+        <header className={`w-full fixed top-0 left-0 right-0 ${isSticky ? "bg-white" : ""}`}>
+          <nav className="py-4 md:px-8 px-4 bg-gradient-to-r from-violet-200 to-purple-100">
+            <div className="flex items-center justify-between">
+              <div className="font-bold text-2xl cursor-pointer text-black">
+                <Link href="/">Donation </Link>
+              </div>
+  
+              <div className="lg:flex items-center gap-3 hidden text-stone-700">
+                <Link href="/" className="block hover:text-green-700 py-2 px-4 font-semibold">
+                  Home
+                </Link>
+                <Link href="/donation" className="block hover:text-green-700 py-2 px-4 font-semibold">
+                  Donation
+                </Link>
+                <Link href="/about" className="block hover:text-green-700 py-2 px-4 font-semibold">
+                  About
+                </Link>
+                <Link href="/statics" className="block hover:text-green-700 py-2 px-4 font-semibold">
+                  Statics
+                </Link>
+              </div>
+  
+              <button onClick={toggleMenu} className="lg:hidden text-red-400 text-3xl">
+                <HiMenu />
+              </button>
+            </div>
+  
+            {isMenuOpen && (
+              <div className="mt-4 bg-green-600 text-white rounded py-4">
+                <Link href="/" className="block hover:text-gray-400 py-2 px-4">
+                  Home
+                </Link>
+                <Link href="/donation" className="block hover:text-gray-400 py-2 px-4">
+                  Donation
+                </Link>
+                <Link href="/about" className="block hover:text-gray-400 py-2 px-4">
+                  About
+                </Link>
+                <Link href="/statics" className="block hover:text-gray-400 py-2 px-4">
+                  Statics
+                </Link>
+                <Link href="/login" className="block hover:text-gray-400 py-2 px-4">
+                  Sign in
+                </Link>
+              </div>
+            )}
+          </nav>
+        </header>
+      </>
+    );
+  }
+
+export default Navbar
