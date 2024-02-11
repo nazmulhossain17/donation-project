c4156d7443efbc5ab261a551599516d7327fa58f
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-02-11 08:29:25 +0600

    *

--- src/app/(withDashboard)/create/page.tsx
+++ src/app/(withDashboard)/create/page.tsx
@@ -74,7 +74,7 @@ const handleRegister = async (data: FormData) => {
                       <input
                         type="text"
                         {...register("image")}
-                        placeholder="Product Name"
+                        placeholder="Image url"
                         className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                       />
                     </div>
